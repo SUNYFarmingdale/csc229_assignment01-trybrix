e738563be1e71a398e19ddf64381e1db83aa9400
Jan <ambo@bos-MacBook-Pro.local>
2024-01-25 16:39:48 -0500

    Made instances and output for the course file

--- src/main/java/com/mycompany/csc229_assignment01/NewClass.java
+++ src/main/java/com/mycompany/csc229_assignment01/NewClass.java
@@ -8,7 +8,18 @@ package com.mycompany.csc229_assignment01;
 public class NewClass {
     
      public static void main(String[] args) {
-        System.out.println("Hello World!");
+         System.out.println("Hello World!");
+
+         Course CRS = new Course();
+            //makes instance of course using default constructor
+
+         Course PCRS = new Course(1229,"Jan",1001);
+            //makes another instance of course but using the overloaded constructor
+
+         System.out.println("My ID is: "+PCRS.getID());
+         System.out.println("My Name is: "+PCRS.getName());
+         System.out.println("My Code is: "+PCRS.getCode());
+            //makes an output for the user
     }
     
 }
